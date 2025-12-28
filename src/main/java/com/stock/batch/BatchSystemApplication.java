561b711b4d4853116a7cad8b866316a854449e7a
gmdcjdakdcjd <ysh010223@naver.com>
2025-12-28 10:48:18 +0900

    init

---
+++ src/main/java/com/stock/batch/BatchSystemApplication.java
@@ -0,0 +1,13 @@
+package com.stock.batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatchSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stock/batch/BatchSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stock.batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
